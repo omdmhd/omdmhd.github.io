303dc83324dbf306a18356a2fe2101825696bbd8
omid mohammadi <omid.mohammadi@sotoon.ir>
2025-02-08 21:15:16 +0330

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static site export
+  basePath: '/omdmhd.github.io', // Replace with your GitHub repo name
 };
 
 export default nextConfig;
